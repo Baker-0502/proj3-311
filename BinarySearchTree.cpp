cfeb903e4c389ba5ec0c171130d33e0a709b17f0
Baker-0502 <bakerdq@email.sc.edu>
2022-11-20 21:33:32 -0500

    Work on multithreading function

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -8,147 +8,169 @@ using namespace std;
 #define OK "OK"
 #define FAIL "Fail"
 
-
-//Node Object, need to make the value field a generic
-class Node {
-    public:
-        int key;
-        string value;
-        string getValue();
-        int getKey();
-        void setValue(string value);
-        void setKey(int key);
-        Node();
-        ~Node();
-        Node(int key, string value);
-        Node *left, *right;
+// Node Object, need to make the value field a generic
+class Node
+{
+public:
+    int key;
+    string value;
+    string getValue();
+    int getKey();
+    void setValue(string value);
+    void setKey(int key);
+    Node();
+    ~Node();
+    Node(int key, string value);
+    Node *left, *right;
 };
 
-Node::Node() {
+Node::Node()
+{
     this->value = "";
     this->key = 0;
     left = right = NULL;
 }
 
-Node::Node(int key, string value){
+Node::Node(int key, string value)
+{
     this->value = value;
     this->key = key;
     left = right = NULL;
 }
 
-Node::~Node() {
+Node::~Node()
+{
     delete left;
     delete right;
 }
 
-string Node::getValue() {
+string Node::getValue()
+{
     return this->value;
 }
 
-int Node::getKey() {
+int Node::getKey()
+{
     return this->key;
 }
 
-void Node::setValue(string value) {
-    this-> value = value;
+void Node::setValue(string value)
+{
+    this->value = value;
 }
 
-void Node::setKey(int key) {
+void Node::setKey(int key)
+{
     this->key = key;
 }
 
-//BST Object
-class BinarySearchTree {
-    public:      
-        Node *root;
-        BinarySearchTree();
-        BinarySearchTree(int key, string value);
-        ~BinarySearchTree();
-        void insert(int key, string value);
-        vector<Node*> search(int key);
-        void remove(int key);
-        void inorder(Node* traverse);
-        Node* findMin(Node* root);
-        void print();
-        void lookup(int key);
-
-
+// BST Object
+class BinarySearchTree
+{
+public:
+    Node *root;
+    BinarySearchTree();
+    BinarySearchTree(int key, string value);
+    ~BinarySearchTree();
+    void insert(int key, string value);
+    vector<Node *> search(int key);
+    void remove(int key);
+    void inorder(Node *traverse);
+    Node *findMin(Node *root);
+    void print();
+    void lookup(int key);
 };
 
 // Default Constructor
 // TODO: Fix default constructor from creating a node that is default to pointing root to null
 // Can be done with a Node, just make sure the node's key and value are NULL and check that
-BinarySearchTree::BinarySearchTree() {
+BinarySearchTree::BinarySearchTree()
+{
     root = NULL;
 }
 
-//Parametrized Constructor
-BinarySearchTree::BinarySearchTree(int key, string value) {
+// Parametrized Constructor
+BinarySearchTree::BinarySearchTree(int key, string value)
+{
     root = new Node(key, value);
 }
 
 // Destructor method
-BinarySearchTree::~BinarySearchTree() {
+BinarySearchTree::~BinarySearchTree()
+{
     delete root;
 }
 
-//Takes a value, finds and deletes from BST, then rearranges (If nessecary)
-//TODO: Fix deletion algorithm, deletes the wrong node or rearranges wrong
+// Takes a value, finds and deletes from BST, then rearranges (If nessecary)
+// TODO: Fix deletion algorithm, deletes the wrong node or rearranges wrong
 
-void BinarySearchTree::remove(int key) {
+void BinarySearchTree::remove(int key)
+{
     // If we have an empty tree, fail and return
-    if (root == NULL) {
+    if (root == NULL)
+    {
         cout << FAIL << endl;
     }
     // Tree isn't empty! Traverse and grab pivot, parent
-    else {
-        vector<Node*> traverse = search(key);
-        Node* pivot = traverse[traverse.size() - 1];
-        Node* parent = traverse[traverse.size() - 2];
+    else
+    {
+        vector<Node *> traverse = search(key);
+        Node *pivot = traverse[traverse.size() - 1];
+        Node *parent = traverse[traverse.size() - 2];
         int tempKey = -1;
         string tempValue = "";
 
-        //If the value is not in the tree
-        if (pivot->getKey() != key) {
+        // If the value is not in the tree
+        if (pivot->getKey() != key)
+        {
             cout << FAIL << endl;
             return;
         }
 
         // If the Node is in the tree, handle the removal by case.
-        else {
+        else
+        {
             // No Leaves
-            if (pivot -> left == NULL && pivot -> right == NULL) {
-                if(pivot -> key < parent -> key) {
-                    parent -> left = NULL;
+            if (pivot->left == NULL && pivot->right == NULL)
+            {
+                if (pivot->key < parent->key)
+                {
+                    parent->left = NULL;
                 }
-                else { parent -> right = NULL; }
+                else
+                {
+                    parent->right = NULL;
+                }
                 delete pivot;
             }
 
             // 1 Leaf (LEFT EMPTY)
-            else if (pivot -> left == NULL && pivot -> right != NULL) {
-                tempKey = pivot -> right -> key;
-                tempValue = pivot -> right -> value;
+            else if (pivot->left == NULL && pivot->right != NULL)
+            {
+                tempKey = pivot->right->key;
+                tempValue = pivot->right->value;
                 pivot->setKey(tempKey);
                 pivot->setValue(tempValue);
             }
 
             // 1 Leaf (RIGHT EMPTY)
             // Need to deep copy the left and right nodes to avoid loss of node after deletion
-            else if (pivot -> right == NULL && pivot -> left != NULL) {
-                tempKey = pivot -> left -> key;
-                tempValue = pivot -> left -> value;
+            else if (pivot->right == NULL && pivot->left != NULL)
+            {
+                tempKey = pivot->left->key;
+                tempValue = pivot->left->value;
                 pivot->setKey(tempKey);
                 pivot->setValue(tempValue);
             }
 
             // 2 Leaves
-            else {
-                Node* temp = findMin(pivot);
-                int smallest = temp -> key;
-                //cout << key;
+            else
+            {
+                Node *temp = findMin(pivot);
+                int smallest = temp->key;
+                // cout << key;
                 remove(smallest);
-                pivot -> value = smallest;
+                pivot->value = smallest;
             }
             cout << "Ok" << endl;
         }
@@ -156,134 +178,162 @@ void BinarySearchTree::remove(int key) {
 }
 
 // Finds the minimum value in the BST
-Node* BinarySearchTree::findMin(Node* root) {
-    if (root -> left == NULL) {
+Node *BinarySearchTree::findMin(Node *root)
+{
+    if (root->left == NULL)
+    {
         return root;
     }
-    else {
-        return findMin(root -> left);
+    else
+    {
+        return findMin(root->left);
     }
 }
 
-
-//Look for the first pos. open (Traverse tree until hit a null that matches BST def. and then sub out null for the new node)
-void BinarySearchTree::insert(int key, string value) {
+// Look for the first pos. open (Traverse tree until hit a null that matches BST def. and then sub out null for the new node)
+void BinarySearchTree::insert(int key, string value)
+{
     // If tree is empty, create a new node at root
-    if (root == NULL) {
+    if (root == NULL)
+    {
         this->root = new Node(key, value);
         cout << OK << endl;
     }
 
-    //Otherwise, traverse the tree to find the appropriate insertion spot
-    else {
-        Node* currNode = root;
+    // Otherwise, traverse the tree to find the appropriate insertion spot
+    else
+    {
+        Node *currNode = root;
         bool search = true;
-        //BinarySearchTree me = *this;
-        while (search) {
-            if (key < currNode -> key) {
-                if (currNode -> left != NULL) {
-                    currNode = currNode -> left;
+        // BinarySearchTree me = *this;
+        while (search)
+        {
+            if (key < currNode->key)
+            {
+                if (currNode->left != NULL)
+                {
+                    currNode = currNode->left;
                     continue;
                 }
-                else {
-                    currNode -> left = new Node(key, value);
+                else
+                {
+                    currNode->left = new Node(key, value);
                     cout << OK << endl;
                     search = false;
                 }
             }
-            if (key > currNode -> key) {
-                if (currNode -> right != NULL) {
-                    currNode = currNode -> right;
+            if (key > currNode->key)
+            {
+                if (currNode->right != NULL)
+                {
+                    currNode = currNode->right;
                     continue;
                 }
-                else {
-                    currNode -> right = new Node(key, value);
+                else
+                {
+                    currNode->right = new Node(key, value);
                     cout << OK << endl;
                     search = false;
                 }
             }
-            else {
+            else
+            {
                 cout << FAIL << endl;
                 search = false;
             }
-        }  
+        }
     }
 }
 
-//Traverse BST using def of BST (i.e left is less, right is more, etc.)
-vector<Node*> BinarySearchTree::search(int key) {
+// Traverse BST using def of BST (i.e left is less, right is more, etc.)
+vector<Node *> BinarySearchTree::search(int key)
+{
     // Start current node at root, create search boolean and traversal vector
-    Node* currNode = root;
+    Node *currNode = root;
     bool search = true;
-    vector<Node*> nodeTraverse;
+    vector<Node *> nodeTraverse;
     // If tree is empty, node will obviously not be in tree, so return
-    if (root == NULL) {
+    if (root == NULL)
+    {
         // cout << "No " << key << endl;
         return nodeTraverse;
     }
-    while (search) {
+    while (search)
+    {
         nodeTraverse.push_back(currNode);
         // If key is less than current nodes key, move left in BST
-        if (key < currNode -> key) {
-            if (currNode -> left != NULL) {
-                currNode = currNode -> left;
+        if (key < currNode->key)
+        {
+            if (currNode->left != NULL)
+            {
+                currNode = currNode->left;
                 continue;
             }
             // Failure to find key
-            else {
+            else
+            {
                 search = false;
                 // cout << "No " << key << endl;
             };
         }
         // If key is greater than current nodes key, move right in BST
-        else if (key > currNode -> key) {
-            if (currNode -> right != NULL) {
-                currNode = currNode -> right;
+        else if (key > currNode->key)
+        {
+            if (currNode->right != NULL)
+            {
+                currNode = currNode->right;
                 continue;
             }
             // Failure to find key
-            else {
+            else
+            {
                 search = false;
                 // cout << "No " << key << endl;
             }
         }
         // Key is found!
-        else {
-            // cout << currNode -> value << endl; 
+        else
+        {
+            // cout << currNode -> value << endl;
             search = false;
-        } 
-    }  
+        }
+    }
     return nodeTraverse;
 }
 
 // Print tree in order
-void BinarySearchTree::print() {
+void BinarySearchTree::print()
+{
     cout << "Printing Tree (In Order!)\n";
     inorder(root);
 }
 
 // Print helper function
-void BinarySearchTree::inorder(Node* traverse) {
-    if(traverse == NULL) {
+void BinarySearchTree::inorder(Node *traverse)
+{
+    if (traverse == NULL)
+    {
         return;
     }
-    inorder(traverse -> left);
-    cout << "Node Key: " << traverse->key <<  "\nValue: " << traverse->value << " \n";
-    inorder(traverse -> right);
+    inorder(traverse->left);
+    cout << "Node Key: " << traverse->key << "\nValue: " << traverse->value << " \n";
+    inorder(traverse->right);
 }
 
 // Lookup Function (Prints to console, so it doesn't break prior search calls)
-void BinarySearchTree::lookup (int key) {
-    vector<Node*> traverse = search(key);
-    Node* pivot = traverse[traverse.size() - 1];
+void BinarySearchTree::lookup(int key)
+{
+    vector<Node *> traverse = search(key);
+    Node *pivot = traverse[traverse.size() - 1];
 
-    //If the value is not in the tree
-    if (pivot->getKey() != key) {
+    // If the value is not in the tree
+    if (pivot->getKey() != key)
+    {
         cout << "No " << key << endl;
         return;
-    
     }
-    else {
+    else
+    {
         cout << pivot->value << endl;
     }
 }
--- Driver.cpp
+++ Driver.cpp
@@ -9,35 +9,45 @@ using namespace std;
 
 /**
  * @brief Takes a C-Style string and parses it into readable string instructions.
- * 
+ *
  * @param line A C-Style String.
  * @return vector<string> Instructions parsed from the line.
  */
-vector<string> processLine(string line) {
+vector<string> processLine(string line)
+{
     vector<string> ret;
     int size = line.length();
     string temp = "";
-    for(int i = 0; i < size; i++) {   
-        if (line[i] != ' ' && line[i] != '\"') {
+    for (int i = 0; i < size; i++)
+    {
+        if (line[i] != ' ' && line[i] != '\"')
+        {
             string s(1, line[i]);
             temp += s;
         }
-        else if (line[i] == '\"') {
+        else if (line[i] == '\"')
+        {
             continue;
         }
-        else {
+        else
+        {
             ret.push_back(temp);
             temp = "";
         }
     }
 
-        ret.push_back(temp);
-        return ret;
+    ret.push_back(temp);
+    return ret;
 }
 
+// We need a function that all threads will run to process input
+void *threadRunner()
+{
 
+}
 
-int main() {
+int main()
+{
     // Init Vars
     string fileName;
     string line;
@@ -45,14 +55,19 @@ int main() {
     vector<string> instructions;
     BinarySearchTree BinTree = BinarySearchTree();
 
-    //Take File Name as Input, Might need to make this a function for multithreaded usage.
-    // TODO: uncomment these 2 lines!
-    // cout << "Enter the name/path of the file: ";
-    // cin >> fileName;
+    // Pthread array
+    pthread_t *threads;
+
+    // Take File Name as Input, Might need to make this a function for multithreaded usage.
+    //  TODO: uncomment these 2 lines!
+    //  cout << "Enter the name/path of the file: ";
+    //  cin >> fileName;
     fileName = "testFile.txt";
     ifstream myFile(fileName);
-    if(myFile.is_open()) {
-        while(myFile.good()) {
+    if (myFile.is_open())
+    {
+        while (myFile.good())
+        {
             getline(myFile, line);
             instructions = processLine(line);
 
@@ -60,41 +75,56 @@ int main() {
             string directive = instructions[0];
             int key = stoi(instructions[1]);
             string value = "";
-            if (instructions.size() > 2) {
+            if (instructions.size() > 2)
+            {
                 value = instructions[2];
             }
             // If the directive is N (Num. Threads), then create N threads to run the process
-            if (directive == "N") {
-                if (key < 1) {
+            if (directive == "N")
+            {
+                if (key < 1)
+                {
                     std::cout << "Cannot have 0 or less threads!" << endl;
                 }
-                else {
-                    numThreads = key;
+                else
+                {
+                    threads = new pthread_t[key];
+                    for (int i = 0; i < key; i++)
+                    {
+                        pthread_create(&threads[i], NULL, &threadRunner, NULL);
+                    }
+
                     std::cout << "Using " << numThreads << " threads." << endl;
                 }
             }
             // If the directive is an I (Insert), Insert the key and value given on that line into the BST
-            else if (directive == "I") {
+            else if (directive == "I")
+            {
                 BinTree.insert(key, value);
             }
             // If the directive is D (Delete), Delete the key given on that line
-            else if (directive == "D") {
+            else if (directive == "D")
+            {
                 BinTree.remove(key);
             }
             // If the directive is L (Lookup/Search), Lookup the key given on that line
-            else if (directive == "L") {
+            else if (directive == "L")
+            {
                 BinTree.lookup(key);
             }
-            else {
+            else
+            {
                 std::cout << "directive not recognized!" << endl;
             }
         }
         myFile.close();
     }
-    
-
 
     // BinTree.print();
-    //test.remove(0);
+    // test.remove(0);
     return 0;
+
+   threadRunner();
+
+   return 0; 
 }
